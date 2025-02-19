9593d8e88ec9188907e0759da62dc890a7576ecd
180cmus <impbpb@icloud.com>
2025-02-19 11:46:28 +0700

    finished project

---
+++ src/main/java/dev/farhan/movies/MovieService.java
@@ -0,0 +1,22 @@
+package dev.farhan.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+}
